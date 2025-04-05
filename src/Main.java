b09b5de73389d66454e295afaafb1ae980451522
josuhun <chosh9128@gmail.com>
2025-04-05 23:47:55 +0900

    [algo] programmers Lv 1. 카드 뭉치

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        String[] cards1 = {"i", "drink", "water"};
-        String[] cards2 = {"want", "to"};
-        String[] goal = {"i", "want", "to", "drink", "water"};
+        String today = "2022.05.19";
+        String[] terms = {"A 6", "B 12", "C 3"};
+        String[] privacies = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
 
-        Solution159994 Solution = new Solution159994();
-        System.out.println(Solution.solution(cards1, cards2, goal));
+        Solution150370 Solution = new Solution150370();
+        System.out.println(Arrays.toString(Solution.solution(today, terms, privacies)));
     }
 }
